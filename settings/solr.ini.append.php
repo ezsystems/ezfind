59d95a538ba8fa61807bfc519ef3bcc97b8f1a4f
Kåre Køhler Høvik <kk@ez.no>
2007-11-27 13:08:35 +0000

    - Removed obsolete settings.
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@2253 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- settings/solr.ini.append.php
+++ settings/solr.ini.append.php
@@ -5,18 +5,5 @@
 SearchServerURI=http://localhost:8983/solr
 # Solr connection timeout in seconds
 ConnectionTimeout=10
-
-[HighLight]
-HighLight=yes
-# If Fields is left empty, all seachable atributes are used
-Fields[]=
-
-[Facets]
-FacetAttributes[]=keywords
-FacetMeta[]=class_name
-FacetMeta[]=published
-FacetMeta[]=modified
-FacetMeta[]=owner_name
-FacetMeta[]=author_name
-
+	
 */ ?>
