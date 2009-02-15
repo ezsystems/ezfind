f7516d35a76d045aff78c08184f6a87386e85d84
Paul Borgermans <pb@ez.no>
2009-02-15 16:36:24 +0000

    - implemented workaround for #013676: Updating Visibility of a node doesn't change search result
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@3581 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- classes/ezfezpsolrquerybuilder.php
+++ classes/ezfezpsolrquerybuilder.php
@@ -142,6 +142,22 @@ class ezfeZPSolrQueryBuilder
             $filterQuery[] = $policyLimitationFilterQuery;
         }
 
+
+
+        if ( (!eZContentObjectTreeNode::showInvisibleNodes() || !$ignoreVisibility ) &&Ê(self::$FindINI->variable( 'SearchFilters', 'FilterHiddenFromDB' ) == 'enabled') )
+        {
+            $db =& eZDB::instance();
+            $invisibleNodeIDArray = $db->arrayQuery( 'SELECT node_id FROM ezcontentobject_tree WHERE ezcontentobject_tree.is_invisible = 1', array( 'column' => 0) );
+            $hiddenNodesQueryText = 'meta_main_node_id_si:[* TO *] -meta_main_node_id_si:(';
+            foreach ( $invisibleNodeIDArray as $element )
+            {
+                $hiddenNodesQueryText =  $hiddenNodesQueryText . $element['node_id'] . ' ';
+            }
+            $hiddenNodesQueryText = $hiddenNodesQueryText . ')';
+            $filterQuery[] = $hiddenNodesQueryText;
+
+        }
+
         // Add content class query filter
         $classLimitationFilter = $this->getContentClassFilterQuery( $contentClassID );
         if ( $classLimitationFilter !== null )
