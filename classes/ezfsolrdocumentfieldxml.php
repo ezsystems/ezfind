a800d9dd6460d716b4de986cc4ce16a3a0d473e0
Nicolas Pastorino <nfrp@ez.no>
2009-02-18 15:30:36 +0000

    --fixed bug #014254: eZMatrix datatype is not searched --fixed bug #014111: Results that contain literal text can create strange output
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@3603 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/ezfsolrdocumentfieldxml.php
@@ -0,0 +1,149 @@
+<?php
+//
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: eZ Find
+// SOFTWARE RELEASE: 2.0.x
+// COPYRIGHT NOTICE: Copyright (C) 2007 eZ Systems AS
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+/*! \file ezfsolrdocumentfieldxml.php
+*/
+
+/*!
+  \class ezfSolrDocumentFieldXML ezfsolrdocumentfieldobjectrelation.php
+  \brief The class ezfSolrDocumentFieldObjectRelation does
+
+*/
+
+class ezfSolrDocumentFieldXML extends ezfSolrDocumentFieldBase
+{
+    public function isCollection()
+    {
+        return false;
+    }
+
+    /**
+     *
+     * @param text $text
+     * @return text
+     *
+     * instead of walking thorugh the dom tree, strip all xml/html like
+     * this is more brute force, but hlps in the case of html literal blocks
+     * which are returned verbatim by ezxml attribute meta data function
+     */
+    public function strip_html_tags( $text )
+    {
+        $text = preg_replace(
+            array(
+            // Replace ezmatrix specific cell and column tags by a space
+            '@<c[^>]*?>(.*?)</c>@siu',
+            '@<column[^>]*?>(.*?)</column>@siu',
+            // Remove invisible content
+            '@<head[^>]*?>.*?</head>@siu',
+            '@<style[^>]*?>.*?</style>@siu',
+            '@<script[^>]*?.*?</script>@siu',
+            '@<object[^>]*?.*?</object>@siu',
+            '@<embed[^>]*?.*?</embed>@siu',
+            '@<applet[^>]*?.*?</applet>@siu',
+            '@<noframes[^>]*?.*?</noframes>@siu',
+            '@<noscript[^>]*?.*?</noscript>@siu',
+            '@<noembed[^>]*?.*?</noembed>@siu',
+            // Add line breaks before and after blocks
+            '@</?((address)|(blockquote)|(center)|(del))@iu',
+            '@</?((div)|(h[1-9])|(ins)|(isindex)|(p)|(pre))@iu',
+            '@</?((dir)|(dl)|(dt)|(dd)|(li)|(menu)|(ol)|(ul))@iu',
+            '@</?((table)|(th)|(td)|(caption))@iu',
+            '@</?((form)|(button)|(fieldset)|(legend)|(input))@iu',
+            '@</?((label)|(select)|(optgroup)|(option)|(textarea))@iu',
+            '@</?((frameset)|(frame)|(iframe))@iu',
+            '@</?(br)@iu'
+            ),
+            array(
+            ' $0 ', ' $0 ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0",
+            "\n\$0", "\n\$0", "\n"
+            ),
+            $text );
+        return strip_tags( $text );
+    }
+
+    /**
+     * Get collection data. Returns list of ezfSolrDocumentFieldBase documents.
+     *
+     * @return array List of ezfSolrDocumentFieldBase objects.
+     */
+    public function getCollectionData()
+    {
+
+    }
+
+    /**
+     * Get data to index, and field name to use. Returns an associative array
+     * with field name and field value.
+     * Example:
+     * <code>
+     * array( 'field_name_i' => 123 );
+     * </code>
+     *
+     * @return array Associative array with fieldname and value.
+     */
+    public function getData()
+    {
+        $contentClassAttribute = $this->ContentObjectAttribute->attribute( 'contentclass_attribute' );
+        $fieldName = self::getFieldName( $contentClassAttribute );
+
+        switch ( $contentClassAttribute->attribute( 'data_type_string' ) )
+        {
+        	case 'ezxmltext' :
+        	{
+                // $xmlData = $this->ContentObjectAttribute->attribute( 'content' )->attribute( 'xml_data' );
+                $xmlData = $this->ContentObjectAttribute->attribute( 'content' )->attribute( 'output' )->attribute( 'output_text' );
+        	} break;
+
+        	case 'ezmatrix' :
+            {
+                $xmlData = $this->ContentObjectAttribute->attribute( 'content' )->xmlString();
+            } break;
+
+            default:
+        	{
+        		return array( $fieldName => '' );
+        	} break;
+        }
+        $cleanedXML = $this->strip_html_tags( $xmlData );
+        return array( $fieldName => $cleanedXML );
+    }
+
+    /**
+     * Get ezfSolrDocumentFieldBase instances for all attributes of specified eZContentObjectVersion
+     *
+     * @param eZContentObjectVersion Instance of eZContentObjectVersion to fetch attributes from.
+     *
+     * @return array List of ezfSolrDocumentFieldBase instances.
+     */
+    function getBaseList( eZContentObjectVersion $objectVersion )
+    {
+
+    }
+}
+
+?>
