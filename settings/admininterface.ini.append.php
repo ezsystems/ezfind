1af11f369d2111a480b7891f83be4bdd5f0b53d1
Nicolas Pastorino <nfrp@ez.no>
2009-02-27 15:03:26 +0000

    --( continued ) merged the 'experimental/nfrp_23012009_elevate_support' into trunk
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@3652 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- settings/admininterface.ini.append.php
+++ settings/admininterface.ini.append.php
@@ -5,10 +5,3 @@ ContextMenuTemplateArray[]=node/ezfindcontextmenu.tpl
 SubitemsContextMenuTemplateArray[]=node/ezfindsubitemscontextmenu.tpl
 SubMenuTemplateArray[]=node/ezfindcontextsubmenu.tpl
 */ ?>
-<?php /* #?ini charset="iso-8859-1"?
-
-[AdditionalMenuSettings]
-ContextMenuTemplateArray[]=node/ezfindcontextmenu.tpl
-SubitemsContextMenuTemplateArray[]=node/ezfindsubitemscontextmenu.tpl
-SubMenuTemplateArray[]=node/ezfindcontextsubmenu.tpl
-*/ ?>
