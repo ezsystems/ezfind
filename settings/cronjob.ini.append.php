4d4d51d85b3f3867c69610890602a2a850b05875
Bertrand Dunogier <bd@ez.no>
2009-04-06 11:47:37 +0000

    Added missing cronjob.ini (references issues #14551 and #14725)
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@3821 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ settings/cronjob.ini.append.php
@@ -0,0 +1,12 @@
+<?php /* #?ini charset="iso-8859-1"?
+
+[CronjobSettings]
+ExtensionDirectories[]=ezfind
+
+[CronjobPart-ezfindexcontent]
+Scripts[]=ezfindexcontent.php
+
+[CronjobPart-ezfoptimizeindex]
+Scripts[]=ezfoptimizeindex.php
+
+*/ ?>
