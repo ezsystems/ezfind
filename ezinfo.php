045d309890f9bc56c0673210b0d6b325747540d2
Paul Borgermans <pb@ez.no>
2009-09-14 17:35:46 +0000

    - updated to reflect rev number of Solr build and rc label of next build
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4171 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- ezinfo.php
+++ ezinfo.php
@@ -39,13 +39,13 @@ class eZFindInfo
     {
         return array(
             'Name' => "eZ Find",
-            'Version' => '2.1.0-alpha1',
+            'Version' => '2.1.0-beta1',
             'Copyright' => "Copyright © 2008-2009 eZ Systems AS.",
             'Info_url' => "http://ez.no",
             'License' => "GNU General Public License v2.0",
             '3rdparty_software' =>
             array ( 'name' => 'Solr',
-                    'Version' => '1.4-dev-rev807925',
+                    'Version' => '1.4-dev-rev 814543',
                     'copyright' => 'The Apache Software Foundation.',
                     'license' => 'Apache License, Version 2.0',
                     'info_url' => 'http://lucene.apache.org/solr/' ) );
