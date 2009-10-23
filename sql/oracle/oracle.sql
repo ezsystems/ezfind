7c14396b0d0d1f138160d5c2820de8638decf0cd
Gaetano Giunta <gg@ez.no>
2009-10-23 13:29:24 +0000

    - Fixed bug: wrong sql file for creating elevation data table for oracle
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4282 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- sql/oracle/oracle.sql
+++ sql/oracle/oracle.sql
@@ -1,7 +1,7 @@
 -- note: the DEFAULT '' NOT NULL are OK, since NOT NULL takes pecendence and we do not rely on ezdbschema to remove the default clause
 
 CREATE TABLE ezfind_elevate_configuration (
-    search_query varchar2(255) NOT NULL default '',
+    search_query varchar2(255) default '' NOT NULL,
     contentobject_id integer default 0 NOT NULL,
     language_code varchar2(20) NOT NULL,
     PRIMARY KEY (search_query, contentobject_id, language_code)
