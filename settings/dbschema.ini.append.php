df3957a802b84a3b82f55b91973d63d470acdfcc
Gaetano Giunta <gg@ez.no>
2010-02-24 22:53:41 +0000

    - Fixed issue #016264: bad index naming in oracle
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4444 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ settings/dbschema.ini.append.php
@@ -0,0 +1,8 @@
+<?php /*
+
+/* shorten index names over 30 chars  for oracle */
+
+[oracle]
+IndexNameTranslation[ezfind_elevate_configuration.ezfind_elevate_configuration__search_query]=ezfind_elevate_config_sq
+
+*/ ?>
