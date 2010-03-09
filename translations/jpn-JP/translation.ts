3ecd0acb4974b4e9530def13fceb6826af6d387d
Eric Sagnes <es@ez.no>
2010-03-09 02:00:29 +0000

    Updated the japanses translation of eZ Find
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4501 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- translations/jpn-JP/translation.ts
+++ translations/jpn-JP/translation.ts
@@ -1,281 +1,228 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="ja_JP">
-<defaultcodec></defaultcodec>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
 <context>
     <name>design/base</name>
     <message>
-        <location filename="" line="0"/>
         <source>The following words were excluded from the search</source>
-        <translation type="unfinished"></translation>
+        <translation>次の言葉は検索から除かれました</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>検索</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>For more options try the %1Advanced search%2</source>
         <comment>The parameters are link start and end tags.</comment>
-        <translation type="unfinished">。</translation>
+        <translation>詳細検索オプションには%1詳細検索%2を使ってください</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>No results were found when searching for &quot;%1&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot;の検索結果がありませんでした</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Search tips</source>
-        <translation type="unfinished"></translation>
+        <translation>検索ティプス</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Check spelling of keywords.</source>
-        <translation type="unfinished"></translation>
+        <translation>キーワードのスペルチェック。</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Try changing some keywords eg. car instead of cars.</source>
-        <translation type="unfinished"></translation>
+        <translation>キーワードを変えてみてください。</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Try more general keywords.</source>
-        <translation type="unfinished"></translation>
+        <translation>一般なキーワードを試してください。</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Fewer keywords gives more results, try reducing keywords until you get a result.</source>
-        <translation type="unfinished"></translation>
+        <translation>少ないキーワードで結果が増えます、結果がでるまでキーワードを減らしてみてください。</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Search for &quot;%1&quot; returned %2 matches</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot;の検索は%2結果を戻しました</translation>
     </message>
 </context>
 <context>
     <name>design/ezwebin/content/search</name>
     <message>
-        <location filename="" line="0"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>検索</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>For more options try the %1Advanced search%2</source>
         <comment>The parameters are link start and end tags.</comment>
-        <translation type="unfinished"></translation>
+        <translation>For more options try the %1Advanced search%2</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>No results were found when searching for &quot;%1&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot;の検索結果がありませんでした。</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Search tips</source>
-        <translation type="unfinished"></translation>
+        <translation>検索ティプス</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Check spelling of keywords.</source>
-        <translation type="unfinished"></translation>
+        <translation>キーワードのスペルチェック。</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Try changing some keywords (eg, &quot;car&quot; instead of &quot;cars&quot;).</source>
-        <translation type="unfinished"></translation>
+        <translation>キーワードを変えてみてください。</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Try searching with less specific keywords.</source>
-        <translation type="unfinished"></translation>
+        <translation>一般なキーワードを試してください。</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Reduce number of keywords to get more results.</source>
-        <translation type="unfinished"></translation>
+        <translation>少ないキーワードで結果が増えます、結果がでるまでキーワードを減らしてみてください。</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Search for &quot;%1&quot; returned %2 matches</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot;の検索は%2結果を戻しました</translation>
     </message>
 </context>
 <context>
     <name>design/standard/content/search</name>
     <message>
-        <location filename="" line="0"/>
         <source>Advanced search</source>
-        <translation type="unfinished"></translation>
+        <translation>詳細検索</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Search all the words</source>
-        <translation type="unfinished"></translation>
+        <translation>すべてのキーワードを検索</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Search the exact phrase</source>
-        <translation type="unfinished"></translation>
+        <translation>文章を検索</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Search with at least one of the words</source>
-        <translation type="unfinished"></translation>
+        <translation>一つのキーワード以上を含めた結果を検索</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Class</source>
-        <translation type="unfinished"></translation>
+        <translation>クラス</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Any class</source>
-        <translation type="unfinished"></translation>
+        <translation>すべてのクラス</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Class attribute</source>
-        <translation type="unfinished"></translation>
+        <translation>クラス属性</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Any attribute</source>
-        <translation type="unfinished"></translation>
+        <translation>すべての属性</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Update attributes</source>
-        <translation type="unfinished"></translation>
+        <translation>更新属性</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>In</source>
-        <translation type="unfinished"></translation>
+        <translation>の中に</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Any section</source>
-        <translation type="unfinished"></translation>
+        <translation>すべてのセクション</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Published</source>
-        <translation type="unfinished"></translation>
+        <translation>公開中</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Any time</source>
-        <translation type="unfinished"></translation>
+        <translation>いつでも</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Last day</source>
-        <translation type="unfinished"></translation>
+        <translation>昨日</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Last week</source>
-        <translation type="unfinished"></translation>
+        <translation>先週</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Last month</source>
-        <translation type="unfinished"></translation>
+        <translation>先月</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Last three months</source>
-        <translation type="unfinished"></translation>
+        <translation>最後の3ヶ月</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Last year</source>
-        <translation type="unfinished"></translation>
+        <translation>去年</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Display per page</source>
-        <translation type="unfinished"></translation>
+        <translation>一頁の結果数</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>5 items</source>
-        <translation type="unfinished"></translation>
+        <translation>5アイテム</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>10 items</source>
-        <translation type="unfinished"></translation>
+        <translation>10アイテム</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>20 items</source>
-        <translation type="unfinished"></translation>
+        <translation>20アイテム</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>30 items</source>
-        <translation type="unfinished"></translation>
+        <translation>30アイテム</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>50 items</source>
-        <translation type="unfinished"></translation>
+        <translation>50アイテム</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>検索</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>No results were found when searching for &quot;%1&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot;の検索結果がありませんでした</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Search for &quot;%1&quot; returned %2 matches</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot;の検索は%2結果を戻しました</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Score</source>
-        <translation type="unfinished"></translation>
+        <translation>スコア</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>名前</translation>
     </message>
 </context>
 <context>
     <name>ezfind</name>
     <message>
-        <location filename="" line="0"/>
         <source>Search time: %1 msecs</source>
         <translation>検索時間：%1ミリ秒</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Core search time: %1 msecs</source>
         <translation>コア検索時間：%1秒</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>N/A</source>
         <translation>該当せず (データがありません)</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>Server not running</source>
         <translation>サ－バ－が作動していません</translation>
     </message>
     <message>
-        <location filename="" line="0"/>
         <source>eZ Find search plugin &amp;copy; 2007 eZ Systems AS, eZ Labs</source>
         <translation>eZ Find search plugin &amp;copy; 2007 eZ Systems AS, eZ Labs</translation>
     </message>
