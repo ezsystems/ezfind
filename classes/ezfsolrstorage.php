70ea62937467ef70e55d0925195834cc1147fbd6
Paul Borgermans <pb@ez.no>
2010-04-22 13:17:23 +0000

    - added initial skeleton API for serializing attribute content
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4584 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/ezfsolrstorage.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @author pb
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ *
+ * @todo: finalize API
+ *
+ * ezfSolrStorage stores serialized versions of attribute content
+ * meta-data from eZ Publish objects is inserted in its stored form already
+ */
+
+class ezfSolrStorage
+{
+    function  __construct()
+    {
+
+    }
+
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @return json encoded string for further processing
+     */
+    public static function serializeAttribute ( eZContentObjectAttribute $contentObjectAttribute )
+    {
+        $target = array(
+            'method' => self::SERIALIZE_METHOD_TOSTRING,
+            'attributedatatype' => $contentObjectAttribute->attribute( 'class_identifier' ),
+            'content' => $contentObjectAttribute->toString()
+                );
+        return json_encode( $target );
+    }
+
+    /**
+     *
+     * @param string $jsonString
+     * @return mixed
+     */
+    public static function unserializeAttribute ( $jsonString )
+    {
+        // primitive for now, it does not return the content in a general usable form yet
+        $attributeContentArray = json_decode( $jsonString, true );
+        return $attributeContentArray['content'];
+    }
+
+    /**
+     *
+     * @param eZContentObjectAttribute $contentObjectAttribute
+     * @return <type> 
+     */
+    public static function getSolrStorageField( eZContentObjectAttribute $contentObjectAttribute )
+    {
+        $classAttribute = $contentObjectAttribute->contentClassAttribute();
+        return array (
+            self::STORAGE_ATTR_FIELD_PREFIX . $classAttribute->attribute('identifier') . self::STORAGE_ATTR_FIELD_SUFFIX =>
+            base64_encode( self::serializeAttribute( $contentObjectAttribute ) )
+
+        );
+    }
+
+    /**
+     *
+     * @param <type> $fieldValue
+     * @return <type> 
+     */
+    public static function decodeSolrStorageFieldValue ( $fieldValue )
+    {
+        return ( self::unserializeAttribute( base64_decode( $fieldValue ) ) );
+    }
+
+    /**
+     *
+     * @param string $fieldName
+     * @param string $fieldValue the encoded field value
+     * @return array with fieldName and the result of deserialize 
+     */
+    public static function decodeSolrStorageField ( $fieldName, $fieldValue )
+    {
+        return ( array( $fieldName, self::decodeSolrStorageFieldValue( $fieldValue ) )  );
+    }
+
+    const STORAGE_ATTR_FIELD_PREFIX = 'as_';
+    const STORAGE_ATTR_FIELD_SUFFIX = '_bst';
+    const SERIALIZE_METHOD_TOSTRING = 'tostring';
+}
+
+?>
