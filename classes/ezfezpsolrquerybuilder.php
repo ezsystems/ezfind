f96cb5d7d4cb26effe1572b4fe29b448de76693d
Paul Borgermans <pb@ez.no>
2010-04-27 14:32:34 +0000

    - fixed bug #016743: meta_name and meta_owner_name are always added to the query
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4592 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- classes/ezfezpsolrquerybuilder.php
+++ classes/ezfezpsolrquerybuilder.php
@@ -301,8 +301,15 @@ class ezfeZPSolrQueryBuilder
         //maybe we should add meta data to the index to filter them out.
 
         $highLightFields = $queryFields;
-        $queryFields[] = eZSolr::getMetaFieldName( 'name' );
-        $queryFields[] = eZSolr::getMetaFieldName( 'owner_name' );
+        
+        //@since eZ Find 2.3
+        //when dedicated attributes are searched for, don't add meta-fields to the $queryfields list
+        if (! $contentClassAttributeID )
+        {
+            $queryFields[] = eZSolr::getMetaFieldName( 'name' );
+            $queryFields[] = eZSolr::getMetaFieldName( 'owner_name' );
+        }
+        
 
         $spellCheckParamList = array();
         // @param $spellCheck expects array (true|false, dictionary identifier, ...)
