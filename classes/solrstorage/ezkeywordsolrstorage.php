5f303e468cfabbf25b79b8c64b6898da59a9ae77
Patrick Allaert <pa@ez.no>
2010-08-05 11:25:19 +0000

    Added: ezkeywordSolrStorage class
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4728 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezkeywordsolrstorage.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * File containing the ezkeywordSolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezkeywordSolrStorage extends ezdatatypeSolrStorage
+{
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
+     * @return array
+     */
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
+    {
+        return array(
+            'content' => $contentObjectAttribute->content()->KeywordArray,
+            'has_rendered_content' => false,
+            'rendered' => null,
+        );
+    }
+}
+
+?>
