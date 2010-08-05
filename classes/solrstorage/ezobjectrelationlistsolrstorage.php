905008a808c67d1955b6b87e0d3a3245faad679c
Patrick Allaert <pa@ez.no>
2010-08-05 11:25:31 +0000

    Added: ezobjectrelationlistSolrStorage class
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4730 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezobjectrelationlistsolrstorage.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * File containing the ezobjectrelationlistSolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezobjectrelationlistSolrStorage extends ezdatatypeSolrStorage
+{
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
+     * @return array
+     */
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
+    {
+        $objectAttributeContent = $contentObjectAttribute->attribute( 'content' );
+        $objectIDList = array();
+        foreach( $objectAttributeContent['relation_list'] as $objectInfo )
+        {
+            $objectIDList[] = $objectInfo['contentobject_id'];
+        }
+
+        return array(
+            'content' => $objectIDList,
+            'has_rendered_content' => false,
+            'rendered' => null,
+        );
+    }
+}
+
+?>
