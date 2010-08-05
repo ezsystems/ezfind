9f4b2f2af535a42af9aaf8d6d110f2eb76e136d0
Patrick Allaert <pa@ez.no>
2010-08-05 11:26:06 +0000

    Added: ezurlSolrStorage class
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4736 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezurlsolrstorage.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * File containing the ezurlSolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezurlSolrStorage extends ezdatatypeSolrStorage
+{
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
+     * @return array
+     */
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
+    {
+        $url = eZURL::fetch( $contentObjectAttribute->attribute( 'data_int' ) );
+        return array(
+            'content' => array(
+                'url' => ($url instanceof eZURL) ? $url->attribute( 'url' ) : null,
+                'text' => $contentObjectAttribute->attribute( 'data_text' ),
+            ),
+            'has_rendered_content' => false,
+            'rendered' => null,
+        );
+    }
+}
+
+?>
