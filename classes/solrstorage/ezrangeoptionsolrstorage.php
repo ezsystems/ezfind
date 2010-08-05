a7f41bc7fb00b2e1fbb97575ae6112de4f669c0f
Patrick Allaert <pa@ez.no>
2010-08-05 11:25:55 +0000

    Added: ezrangeoptionSolrStorage class
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4734 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezrangeoptionsolrstorage.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * File containing the ezrangeoptionSolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezrangeoptionSolrStorage extends ezdatatypeSolrStorage
+{
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
+     * @return array
+     */
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
+    {
+        $option = $contentObjectAttribute->attribute( 'content' );
+
+        return array(
+            'content' => array(
+                'name' => $option->attribute( 'name' ),
+                'start_value' => $option->attribute( 'start_value' ),
+                'stop_value' => $option->attribute( 'stop_value' ),
+                'step_value' => $option->attribute( 'step_value' ),
+            ),
+            'has_rendered_content' => false,
+            'rendered' => null,
+        );
+    }
+}
+
+?>
