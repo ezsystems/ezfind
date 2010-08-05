54a26c29eaf246d1b25d7dc14f21db7c1bb0aa24
Patrick Allaert <pa@ez.no>
2010-08-05 11:25:48 +0000

    Added: ezproductcategorySolrStorage class
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4733 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezproductcategorysolrstorage.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * File containing the ezproductcategorySolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezproductcategorySolrStorage extends ezdatatypeSolrStorage
+{
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
+     * @return array
+     */
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
+    {
+        $category =  $contentObjectAttribute->attribute( 'content' );
+
+        return array(
+            'content' => array(
+                'name' => $category ? $category->attribute( 'name' ) : null,
+                'id' => $category ? $category->attribute( 'id' ) : null,
+            ),
+            'has_rendered_content' => false,
+            'rendered' => null,
+        );
+    }
+}
+
+?>
