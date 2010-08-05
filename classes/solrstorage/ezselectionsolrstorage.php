e50f6f3b610785737fbb8de46bf5f3f1816b2146
Patrick Allaert <pa@ez.no>
2010-08-05 11:26:00 +0000

    Added: ezselectionSolrStorage class
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4735 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezselectionsolrstorage.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * File containing the ezselectionSolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezselectionSolrStorage extends ezdatatypeSolrStorage
+{
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
+     * @return array
+     */
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
+    {
+        $selectedOptionsList   = array_fill_keys( $this->contentObjectAttribute->content(), true );
+        $availableOptionsArray = $this->contentObjectAttribute->attribute( 'class_content' );
+        $finalAvailableOptions = array();
+
+        foreach ( $availableOptionsArray['options'] as $availableOption )
+        {
+            if ( isset( $selectedOptionsList[$availableOption['id']] ) )
+            {
+                $finalAvailableOptions[] = array( 'name' => $availableOption['name'], 'id' => $availableOption['id'] );
+            }
+        }
+        return array(
+            'content' => $finalAvailableOptions,
+            'has_rendered_content' => false,
+            'rendered' => null,
+        );
+    }
+}
+
+?>
