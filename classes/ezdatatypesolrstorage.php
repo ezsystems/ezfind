e68aeaa535e94666179be5ad758857a2f3552b15
Patrick Allaert <pa@ez.no>
2010-08-05 11:25:01 +0000

    Changed: standardizing somewhat getAttributeContent() signature and doc
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4725 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- classes/ezdatatypesolrstorage.php
+++ classes/ezdatatypesolrstorage.php
@@ -13,11 +13,12 @@ class ezdatatypeSolrStorage
 {
     /**
      * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
      * @return array with keys 'content', 'has_rendered_content', 'rendered'
      * required first level elements 'method', 'version_format', 'data_type_identifier', 'content'
      * optional first level element is 'rendered' which should store (template) rendered xhtml snippets
      */
-    public static function getAttributeContent ( eZContentObjectAttribute $contentObjectAttribute, $contentClassAttribute )
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
     {
 
         $target = array(
--- classes/solrstorage/ezmatrixsolrstorage.php
+++ classes/solrstorage/ezmatrixsolrstorage.php
@@ -14,11 +14,12 @@ class ezmatrixSolrStorage extends ezdatatypeSolrStorage
 
     /**
      * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
      * @return json encoded string for further processing
      * required first level elements 'method', 'version_format', 'data_type_identifier', 'content'
      * optional first level element is 'rendered' which should store (template) rendered xhtml snippets
      */
-    public static function getAttributeContent(eZContentObjectAttribute $contentObjectAttribute, $contentClassAttribute)
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute)
     {
 
         
