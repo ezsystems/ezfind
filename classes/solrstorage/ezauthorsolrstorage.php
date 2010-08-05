f44c2ecd7096722880b442fc23f15fe8781fe8c2
Patrick Allaert <pa@ez.no>
2010-08-05 11:25:07 +0000

    Added: ezauthorSolrStorage class
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4726 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezauthorsolrstorage.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * File containing the ezauthorSolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezauthorSolrStorage extends ezdatatypeSolrStorage
+{
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
+     * @return array
+     */
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
+    {
+        $authorList = array();
+        foreach ( $contentObjectAttribute->attribute( 'content' )->attribute( 'author_list' ) as $author )
+        {   
+            $authorList[] = array(
+                'id' => $author['id'],
+                'name' => $author['name'],
+                'email' => $author['email'],
+            );
+        }
+
+        return array(
+            'content' => $authorList,
+            'has_rendered_content' => false,
+            'rendered' => null,
+        );
+    }
+}
+
+?>
