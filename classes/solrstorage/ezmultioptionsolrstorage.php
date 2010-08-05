406a48bff76c05748198214e70ed8e04abd04313
Patrick Allaert <pa@ez.no>
2010-08-05 11:25:25 +0000

    Added: ezmultioptionSolrStorage class
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4729 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezmultioptionsolrstorage.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * File containing the ezmultioptionSolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezmultioptionSolrStorage extends ezdatatypeSolrStorage
+{
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
+     * @return array
+     */
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
+    {
+        $content = $contentObjectAttribute->attribute( 'content' );
+        $multioptionArray = array(
+            'name' => $content->attribute( 'name' ),
+        );
+
+        foreach ( $content->attribute( 'multioption_list' ) as $option )
+        {   
+            $optionArray = array(
+                'name' => $option['name'],
+                'default_option_id' => $option['default_option_id']
+            );
+            foreach ( $option['optionlist'] as $value )
+            {   
+                $optionArray['optionlist'][] = array(
+                    'value' => $value['value'],
+                    'additional_price' => $value['additional_price']
+                );
+            }   
+            $multioptionArray['multioption_list'][] = $optionArray;
+        }
+
+        return array(
+            'content' => $multioptionArray,
+            'has_rendered_content' => false,
+            'rendered' => null,
+        );
+    }
+}
+
+?>
