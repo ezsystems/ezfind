2acdc13aad1dccd863abdca8e3856c16f5c11f23
Patrick Allaert <pa@ez.no>
2010-08-05 11:25:13 +0000

    Added: ezenumSolrStorage class
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4727 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezenumsolrstorage.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * File containing the ezenumSolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezenumSolrStorage extends ezdatatypeSolrStorage
+{
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
+     * @return array
+     */
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
+    {
+        $availableEnumerations = array();
+        foreach ( $contentObjectAttribute->content()->Enumerations  as $enumeration )
+        {
+            $availableEnumerations[] = array(
+                'id' => $enumeration->EnumID,
+                'element' => $enumeration->EnumElement,
+                'value' => $enumeration->EnumValue
+            );
+        }
+
+        return array(
+            'content' => $availableEnumerations,
+            'has_rendered_content' => false,
+            'rendered' => null,
+        );
+    }
+}
+
+?>
