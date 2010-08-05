25c2670228df51e960f916ef3aa1625090ef26ba
Patrick Allaert <pa@ez.no>
2010-08-05 11:25:42 +0000

    Added: ezpriceSolrStorage class
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4732 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezpricesolrstorage.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * File containing the ezpriceSolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezpriceSolrStorage extends ezdatatypeSolrStorage
+{
+    /**
+     * @param eZContentObjectAttribute $contentObjectAttribute the attribute to serialize
+     * @param eZContentClassAttribute $contentClassAttribute the content class of the attribute to serialize
+     * @return array
+     */
+    public static function getAttributeContent( eZContentObjectAttribute $contentObjectAttribute, eZContentClassAttribute $contentClassAttribute )
+    {
+        $price = $contentObjectAttribute->attribute( 'content' );
+
+        return array(
+            'content' => array(
+                'price' => $price->attribute( 'price' ),
+                'selected_vat_type' => $price->attribute( 'selected_vat_type' )->attribute( 'id' ),
+                'is_vat_included' => (bool)$price->attribute( 'is_vat_included' ),
+            ),
+            'has_rendered_content' => false,
+            'rendered' => null,
+        );
+    }
+}
+
+?>
