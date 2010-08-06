55e265969a7aaaba65369537482b70a2c984dc08
Patrick Allaert <pa@ez.no>
2010-08-06 09:11:11 +0000

    Added: ezdateSolrStorage
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4741 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ classes/solrstorage/ezdatesolrstorage.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * File containing the ezdateSolrStorage class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ * @package ezfind
+ */
+
+class ezdateSolrStorage extends ezdatetimeSolrStorage
+{
+}
+
+?>
