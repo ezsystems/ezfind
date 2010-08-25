7c5f55012373558f2299f9d30df780889fad946b
Paul Borgermans <pb@ez.no>
2010-08-25 15:06:27 +0000

    - Fixed bug #016532: dbschema.ini not a valid php file
    git-svn-id: http://svn.ez.no/svn/extensions/ezfind/ezp4/trunk@4755 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- settings/dbschema.ini.append.php
+++ settings/dbschema.ini.append.php
@@ -1,6 +1,6 @@
 <?php /*
 
-// shorten index names over 30 chars for oracle
+# shorten index names over 30 chars for oracle
 
 [oracle]
 IndexNameTranslation[ezfind_elevate_configuration.ezfind_elevate_configuration__search_query]=ezfind_elevate_config_sq
