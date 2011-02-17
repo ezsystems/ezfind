a533e7fd5b08b81ceb6d08b2b26477512377c823
lolautruche <lolautruche@gmail.com>
2011-02-17 13:49:44 +0100

    Fixed bug #017862: eZ Find indexers should retry failed objects

---
+++ tests/suite.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * File containing the eZFindTestSuite class
+ *
+ * @copyright Copyright (C) 1999-2009 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ * @package tests
+ */
+
+class eZFindTestSuite extends ezpDatabaseTestSuite
+{
+    public function __construct()
+    {
+        parent::__construct();
+        ini_set( 'xdebug.show_exception_trace', 'Off' );
+        $this->setName( "eZ Find Test Suite" );
+
+        $this->addTestSuite( 'eZSolrBaseRegression' );
+    }
+
+    public static function suite()
+    {
+        return new self();
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // make sure extension is enabled and settings are read
+        ezpExtensionHelper::load( 'ezfind' );
+    }
+
+    public function tearDown()
+    {
+        ezpExtensionHelper::unload( 'ezfind' );
+        parent::tearDown();
+    }
+}
+
+?>
