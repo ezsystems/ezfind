6e9cf2a6662f87cf6597dc663734c58138c662c9
Damien Pobel <dp@ez.no>
2011-08-05 16:15:54 +0200

    Fixed #18468: moreLikeThis query, when passed a node ID or object ID, does not specify an installation ID
    Also make possible to specify the installation id to use to search for
    the reference document in the Solr when using the template fetch
    function. Based on the pull request #13 from Peter Keung.

--- classes/ezfezpsolrquerybuilder.php
+++ classes/ezfezpsolrquerybuilder.php
@@ -668,6 +668,7 @@ class ezfeZPSolrQueryBuilder
         eZDebug::writeDebug( $params, 'mlt params' );
         $searchCount = 0;
 
+        $queryInstallationID = ( isset( $params['QueryInstallationID'] ) && $params['QueryInstallationID'] ) ? $params['QueryInstallationID'] : eZSolr::installationID();
         $offset = ( isset( $params['SearchOffset'] ) && $params['SearchOffset'] ) ? $params['SearchOffset'] : 0;
         $limit = ( isset( $params['SearchLimit']  ) && $params['SearchLimit'] ) ? $params['SearchLimit'] : 10;
         $subtrees = isset( $params['SearchSubTreeArray'] ) ? $params['SearchSubTreeArray'] : array();
@@ -756,10 +757,12 @@ class ezfeZPSolrQueryBuilder
         switch ( strtolower ($queryType) )
         {
             case 'nid':
-                $mltQuery = eZSolr::getMetaFieldName( 'node_id') . ':' . $query;
+                $mltQuery = eZSolr::getMetaFieldName( 'node_id' ) . ':' . $query;
+                $mltQuery .= ' AND ' . eZSolr::getMetaFieldName( 'installation_id' ) . ':' . $queryInstallationID;
                 break;
             case 'oid':
-                $mltQuery = eZSolr::getMetaFieldName( 'id') . ':' . $query;
+                $mltQuery = eZSolr::getMetaFieldName( 'id' ) . ':' . $query;
+                $mltQuery .= ' AND ' . eZSolr::getMetaFieldName( 'installation_id' ) . ':' . $queryInstallationID;
                 break;
             case 'url':
                 $mltVariant = 'stream.url';
