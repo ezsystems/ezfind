a6a32c7a406b42971cfa85917336de998a2d5e45
Damien Pobel <dp@ez.no>
2011-08-16 10:30:11 +0200

    Fixed eZ Find and Solr version numbers

--- ezinfo.php
+++ ezinfo.php
@@ -45,7 +45,7 @@ class eZFindInfo
             'License' => "GNU General Public License v2.0",
             '3rdparty_software' =>
                             array ( 'name' => 'Solr',
-                                    'Version' => '1.5-dev-rev 925669',
+                                    'Version' => '3.1',
                                     'copyright' => 'The Apache Software Foundation.',
                                     'license' => 'Apache License, Version 2.0',
                                     'info_url' => 'http://lucene.apache.org/solr/' ) );
