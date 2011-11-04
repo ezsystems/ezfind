f95b62cdaeefff9a65edba1fab62da67700ce2f6
Jérôme Vieilledent <lolautruche@gmail.com>
2011-11-04 18:19:39 +0100

    Fixed #15722: eZ Find ignores ignore_visibility parameter (cherry picked from commit cb865acf34a08b5feb07b41e87ec6d1df6d4672e) (cherry picked from commit b019f93dba388313c1e0a0e2c4d15206a79eb7bf)

--- settings/cronjob.ini.append.php
+++ settings/cronjob.ini.append.php
@@ -9,4 +9,10 @@ Scripts[]=ezfindexcontent.php
 [CronjobPart-ezfoptimizeindex]
 Scripts[]=ezfoptimizeindex.php
 
+[CronjobPart-frequent]
+Scripts[]=ezfindexsubtree.php
+
+[CronjobPart-ezfindexsubtree]
+Scripts[]=ezfindexsubtree.php
+
 */ ?>
