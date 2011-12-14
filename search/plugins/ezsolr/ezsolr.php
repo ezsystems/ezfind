097016e6dcb8f05022acd49e8f12ebba8b566ed7
Patrick Allaert <pa@ez.no>
2011-12-14 11:58:24 +0100

    Fixed #018777: Delayed indexing fails when adding another location to a node (cherry picked from commit cd8d7b21b6529ca276c375732c1dbc36bfacf840)

--- search/plugins/ezsolr/ezsolr.php
+++ search/plugins/ezsolr/ezsolr.php
@@ -1472,8 +1472,7 @@ class eZSolr implements ezpSearchEngine
      */
     public function addNodeAssignment( $mainNodeID, $objectID, $nodeAssignmentIDList )
     {
-        $contentObject = eZContentObject::fetch( $objectID );
-        $this->addObject( $contentObject );
+        eZContentOperationCollection::registerSearchObject( $objectID );
     }
 
     /**
