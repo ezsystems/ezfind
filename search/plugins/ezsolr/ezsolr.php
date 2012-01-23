3e0d1e91f14dc4406b138b942372e37466dfed38
Jérôme Vieilledent <lolautruche@gmail.com>
2012-01-23 12:12:34 +0100

    Fixed #019071: eZFind permanently throws errors, because of subtree limitations (cherry picked from commit fc7855a03cedd055a90346003d0f4ff216a8e7f7)

--- search/plugins/ezsolr/ezsolr.php
+++ search/plugins/ezsolr/ezsolr.php
@@ -377,12 +377,16 @@ class eZSolr implements ezpSearchEngine
         else
         {
             $contentId = $doc[ezfSolrDocumentFieldBase::generateMetaFieldName( 'id' )];
-            eZDebug::writeError(
-                "Could not find valid/granted locations for content #$contentId. Broken sync between eZPublish and Solr ?\n\n" .
-                "Location filter : " . print_r( $locationFilter, true ) .
-                "Subtree limitations for user : " . print_r( $subtreeLimitations, true ),
-                __METHOD__
-            );
+            $content = eZContentObject::fetch( $contentId );
+            if ( $content instanceof eZContentObject && !$content->canRead() )
+            {
+                eZDebug::writeError(
+                    "Could not find valid/granted locations for content #$contentId. Broken sync between eZPublish and Solr ?\n\n" .
+                    "Location filter : " . print_r( $locationFilter, true ) .
+                    "Subtree limitations for user : " . print_r( $subtreeLimitations, true ),
+                    __METHOD__
+                );
+            }
         }
 
         return (int)$doc[ezfSolrDocumentFieldBase::generateMetaFieldName( 'main_node_id' )];
