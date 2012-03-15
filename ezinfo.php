cf7252d48d1ef12f39fc30c8f87bf9f578058d19
Paul Borgermans <paul.borgermans@gmail.com>
2012-03-15 10:24:51 +0100

    Updated version numbers in extension meta-data

--- ezinfo.php
+++ ezinfo.php
@@ -45,7 +45,7 @@ class eZFindInfo
             'License' => "GNU General Public License v2.0",
             '3rdparty_software' =>
                             array ( 'name' => 'Solr',
-                                    'Version' => '3.1',
+                                    'Version' => '3.5',
                                     'copyright' => 'The Apache Software Foundation.',
                                     'license' => 'Apache License, Version 2.0',
                                     'info_url' => 'http://lucene.apache.org/solr/' ) );
