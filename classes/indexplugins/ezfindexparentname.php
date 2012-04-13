b18f60b854a4f6d4af2b1ed33518927eabea24f2
Bertrand Dunogier <bd@ez.no>
2012-04-13 17:03:19 +0200

    Copyright strings fixes (cherry picked from commit c30a04cfdd094a1f1f1f22780ee63a7ebeb00d8d)

--- classes/indexplugins/ezfindexparentname.php
+++ classes/indexplugins/ezfindexparentname.php
@@ -3,7 +3,7 @@
 /*
  * File containing example index plugin class
  *
- * @copyright Copyright (C) 2012 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 2012-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
  * @package ezfind
