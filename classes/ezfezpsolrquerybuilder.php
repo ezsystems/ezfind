a9ea041be56a25f04a0a9f8fa27acffd143f0430
Jérôme Gamez <jerome.gamez@gmail.com>
2014-01-23 12:58:31 +0100

    EZP-21332: If field name couldn't be retrieved, use base field name

--- classes/ezfezpsolrquerybuilder.php
+++ classes/ezfezpsolrquerybuilder.php
@@ -942,11 +942,12 @@ class ezfeZPSolrQueryBuilder
 
                     default:
                     {
+                        $baseFieldName = $field;
                         $field = eZSolr::getFieldName( $field, false, 'sort' );
                         if ( !$field )
                         {
                             eZDebug::writeNotice( 'Sort field does not exist in local installation, but may still be valid: ' .
-                                                  $facetDefinition['field'],
+                                                  $baseFieldName,
                                                   __METHOD__ );
                             continue;
                         }
