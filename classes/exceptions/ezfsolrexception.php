7bc727d9e3cbad3fbf0adba5403c32f7da33dc67
André R <ar@ez.no>
2014-05-09 20:52:50 +0200

    Fix EZP-22128: Change license & copyright info to refer to root file
    Update inline copyright and license info to refer to LICENSE file.
    Makes it easier to maintain source code across licenses and easier
    to keep on updating the copyright year.
    
    The format here is similar as used in Symfony.

--- classes/exceptions/ezfsolrexception.php
+++ classes/exceptions/ezfsolrexception.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * File containing ezfSolrException class
- * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @author jv
- * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
  * @version //autogentag//
  * @package ezfind
  *
--- classes/ezfindextendedattributefilterinterface.php
+++ classes/ezfindextendedattributefilterinterface.php
@@ -3,7 +3,7 @@
  * Extended attribute filter interface
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @author bchoquet
- * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
  * @version //autogentag//
  * @package ezfind
  */
